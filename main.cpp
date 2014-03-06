8de098976232369b5790bcc1cda93288c87c013c
baba <wen.max8@gmail.com>
2014-03-07 06:01:41 +0800

    1. log dispatch

--- main.cpp
+++ main.cpp
@@ -247,7 +247,7 @@ int main(int argc, const char * argv[])
 		static uint32_t pos = 0;
 		auto &endpoint = (*endpoints)[++pos % endpoints->size()];
 //		std::cout << endpoint.address() << ":" << endpoint.port() << std::endl;
-		print_info("dipatch to %s:%u\n", endpoint.address().c_str(), endpoint.port());
+		print_info("dipatch to %s:%u\n", endpoint.address().to_v4().to_string().c_str(), endpoint.port());
 		static auto t0 = time(nullptr);
 		static uint32_t counter = 0;
 		++counter;
